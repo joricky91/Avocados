85aec5aaa17b0d9ddd27be336f8256bd78f6fc30
joricky91 <jonathanricky91@gmail.com>
2024-01-16 16:33:03 +0700

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Jonathan Ricky Sandjaja on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
