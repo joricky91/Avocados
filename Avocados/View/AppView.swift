2687bc094027fa546f0063add4055bb51e45d84f
joricky91 <jonathanricky91@gmail.com>
2024-01-19 14:42:00 +0700

    feat: tab view

--- Avocados/View/AppView.swift
+++ Avocados/View/AppView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            AvocadosView()
+                .tabItem {
+                    Image("tabicon-branch")
+                    
+                    Text("Avocados")
+                }
+            
+            ContentView()
+                .tabItem {
+                    Image("tabicon-book")
+                    
+                    Text("Recipes")
+                }
+            
+            RipeningStagesView()
+                .tabItem {
+                    Image("tabicon-avocado")
+                    
+                    Text("Ripening")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Image("tabicon-settings")
+                    
+                    Text("Settings")
+                }
+        }
+        .ignoresSafeArea(edges: .top)
+        .accentColor(Color.primary)
+        .onAppear {
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithDefaultBackground()
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        }
     }
 }
 
