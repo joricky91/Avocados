2da3a4e4e8117e60fbfe7c1f15bc741dac56cf73
joricky91 <jonathanricky91@gmail.com>
2024-01-23 17:18:57 +0700

    feat: finishing dishes view

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -22,13 +22,20 @@ struct ContentView: View {
                     }
                 }
                 
+                //MARK: - DISHES
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+                    .frame(maxWidth: 640)
+                
                 //MARK: - FOOTER
                 VStack(alignment: .center, spacing: 20) {
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
+                        .frame(minHeight: 60)
                     
                     Text("Everything you wanted to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
@@ -42,6 +49,15 @@ struct ContentView: View {
         }
         .ignoresSafeArea()
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
